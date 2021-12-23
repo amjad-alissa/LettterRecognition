fb545df90e701d9bb1c1c8b9e73eacb1c49c2aa3
amjad25-bit <amjad-alissa@outlook.com>
2021-12-23 17:02:10 +0100

    first commit

---
+++ gui/Main.java
@@ -0,0 +1,48 @@
+// Amjad Alissa Alkhalaf
+// Rami Bader
+
+package gui;
+
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Orientation;
+import javafx.scene.Scene;
+import javafx.scene.control.Separator;
+import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    public static void main(String[] args) {
+
+        launch(args);
+    }
+
+    public void start(Stage myStage) {
+
+        //Stage title
+        myStage.setTitle("Letters Recognition");
+
+        //instances of the classes
+        EnlargedCanvas enlargedCanvas = new EnlargedCanvas();
+        Buttons buttons = new Buttons();
+
+        Separator separator = new Separator(Orientation.VERTICAL);
+
+        //HBox pane
+        HBox rootNode = new HBox(10);
+        rootNode.setPadding(new Insets(10));
+        rootNode.getStylesheets().add(getClass().getResource("StyleSheet.css").toString());
+        rootNode.getChildren().addAll(buttons,separator, enlargedCanvas);
+
+        //Creating and Setting the scence
+        Scene myScene = new Scene(rootNode,rootNode.getMinWidth(),rootNode.getMinHeight());
+
+        //Setting the stage
+        myStage.setScene(myScene);
+        myStage.setResizable(false);
+        myStage.sizeToScene();
+        myStage.show();
+    }
+}
+
